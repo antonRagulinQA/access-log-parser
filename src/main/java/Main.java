88fb7420e6c4fc18d90db7bc0bfee9f39f758dc0
Anton Ragulin <git.user>
2025-07-02 09:26:28 +0300

    Добавлен функционал по расчету произведения, деления, разнсоти и сложения двух чисел

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        int secondNumber = new Scanner(System.in).nextInt();
-        int multiply = (firstNumber * secondNumber);
-        System.out.println("Произведение чисел: " + multiply);
-        int sum = (firstNumber + secondNumber);
-        System.out.println("Сумма чисел: " + sum);
-        int minus  = (firstNumber - secondNumber);
-        System.out.println("Сумма чисел: " + sum);
-        double divide = (double) (firstNumber + secondNumber);
-        System.out.println("Частное двух чисел: "+ divide);
-
-
-    }
-}
+                System.out.println("Введите число 1 :");
+                int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите число 2 :");
+                int secondNumber = new Scanner(System.in).nextInt();
+                int multiply = (firstNumber * secondNumber);
+                System.out.println("Произведение чисел: " + multiply);
+                int sum = (firstNumber + secondNumber);
+                System.out.println("Сумма чисел: " + sum);
+                int minus  = (firstNumber - secondNumber);
+                System.out.println("Разность чисел: " + minus);
+                double divide = (double) (firstNumber / secondNumber);
+                System.out.println("Частное двух чисел: "+ divide);
+            }
+        }
